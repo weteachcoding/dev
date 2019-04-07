0b4dcbc57a8100bf7d2ec8962adce8fe9575cf75
weteachcoding <48222183+weteachcoding@users.noreply.github.com>
2019-04-07 15:07:52 +0100

    Add files via upload

---
+++ wish.js
@@ -0,0 +1,78 @@
+$(window).load(function(){
+	
+	$(".preloader").fadeOut(slow)
+	
+});
+
+$("button#wish1").click(function(){
+	
+	
+
+	
+
+
+	
+		$("audio")[0].pause();
+		
+		$("img").slideDown(8000).attr("src", "myimage/2.jpg");
+	$("img").slideDown(8000).attr("src", "myimage/3.jpg").slideUp(8000).fadeIn(8000).fadeOut(8000).fadeIn(8000);
+
+$("button").slideUp(8000).slideDown(8000).slideUp(8000).slideDown(8000).slideUp(8000).slideDown(8000).slideUp(8000).slideDown(8000);
+	$("audio#pop1")[0].play()
+	
+	$("h3").slideDown(8000).slideUp(8000).slideDown(8000).text("I love you and promise to love you with everything I have.");
+		
+});
+
+$("button#wish2").click(function(){
+	
+	$("audio")[0].pause();
+		$("img").slideDown(8000).attr("src", "myimage/2.jpg");
+			$("img").slideDown(8000).attr("src", "myimage/3.jpg").slideUp(8000).fadeIn(8000).fadeOut(8000).fadeIn(8000);
+		
+		
+	$("button").slideUp(8000).slideDown(8000).slideUp(8000).slideDown(8000).slideUp(8000).slideDown(8000).slideUp(8000).slideDown(8000);
+	
+	$("audio#pop2")[0].play();
+	
+	$("h3").slideUp().slideDown().text("I will forever love you, even if you don't have money. I promise  ")
+});
+
+$("button#wish3").click(function(){
+		$("audio")[0].pause();
+	$("button").slideUp(8000).slideDown(8000).slideUp(8000).slideDown(8000).slideUp(8000).slideDown(8000).slideUp(4000).slideDown(8000);
+	
+	$("audio#pop3")[0].play();
+	
+		
+});
+		
+
+
+$("button#wish4").click(function(){
+		
+	$("audio")[0].pause();
+	$("button").slideUp(8000).slideDown(8000).slideUp(8000).slideDown(8000).slideUp(8000).slideDown(8000).slideUp(8000).slideDown(8000);
+	$("h3").slideUp().slideDown().text("I miss your kiss, your hug, your words, I miss everything about you.  My eyes really missed you. ");
+
+
+	$("audio#pop4")[0].play();
+	
+			
+		
+});
+$("button#wish5").click(function(){
+	
+		$("audio")[0].pause();
+	$("button").slideUp(8000).slideDown(8000).slideUp(8000).slideDown(8000).slideUp(8000).slideDown(8000).slideUp(8000).slideDown(8000);
+	
+		
+	$("h3").slideUp(8000).slideDown(8000).slideUp(8000).slideDown(8000).slideUp(8000).slideDown(8000).slideUp(8000).slideDown(8000).text("I pray your hustle will pay, all what makes you sad will eventually turn to success, no more sadness my heart, hustle will soon pay");
+
+	
+	$("audio#pop5")[0].play();
+	
+			
+
+		
+});
